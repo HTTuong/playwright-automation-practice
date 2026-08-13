7892e782cbf053e9f3e10cba2eac4355d2e26219
Tuong Hoang <httuong.2705@gmail.com>
2026-08-13 22:45:45 +0300

    Refactor cart flow test with custom fixture

--- tests/cart-flow.spec.ts
+++ tests/cart-flow.spec.ts
@@ -1,44 +1,33 @@
-import { test, expect } from '@playwright/test';
-import LoginPage  from '../pages/LoginPage';
-import  InventoryPage from '../pages/InventoryPage';
+import { test } from '../fixtures/pages.fixture';
 import  CartPage  from '../pages/CartPage';
 
 
 test.describe('Cart flow', () => {
-    let inventoryPage: InventoryPage;
-    let cartPage: CartPage;
-
-    test.beforeEach(async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.goto();
-        await loginPage.login('standard_user', 'secret_sauce');
-        inventoryPage = new InventoryPage(page);
-        cartPage = new CartPage(page);
-    });
-
-    test('Add 1 product to cart', async ({page}) => {
-        await inventoryPage.addFirstItemToCart()
-        await inventoryPage.expectCartBadgeCount('1')
+    test('Add 1 product to cart', async ({loggedInPage}) => {
+        await loggedInPage.addFirstItemToCart()
+        await loggedInPage.expectCartBadgeCount('1')
     })
 
-    test('Add 3 product to cart', async ({page}) => {
-        await inventoryPage.addItemToCartByIndex(0);
-        await inventoryPage.addItemToCartByIndex(1);
-        await inventoryPage.addItemToCartByIndex(2);
-        await inventoryPage.expectCartBadgeCount('3');
+    test('Add 3 product to cart', async ({loggedInPage}) => {
+        await loggedInPage.addItemToCartByIndex(0);
+        await loggedInPage.addItemToCartByIndex(1);
+        await loggedInPage.addItemToCartByIndex(2);
+        await loggedInPage.expectCartBadgeCount('3');
     })
 
-    test('Remove product in cart', async ({ page }) => {
-        await inventoryPage.addFirstItemToCart()
-        await inventoryPage.goToCart()
+    test('Remove product in cart', async ({ loggedInPage, page }) => {
+        await loggedInPage.addFirstItemToCart()
+        await loggedInPage.goToCart()
+        const cartPage = new CartPage(page);
         await cartPage.removeFirstItem()
-        await inventoryPage.expectCartBadgeHidden()
+        await loggedInPage.expectCartBadgeHidden()
     });
 
-    test('Check added product in cart', async ({ page }) => {
-        const productName = await inventoryPage.getFirstItemName()
-        await inventoryPage.addFirstItemToCart()
-        await inventoryPage.goToCart()
+    test('Check added product in cart', async ({ loggedInPage, page }) => {
+        const productName = await loggedInPage.getFirstItemName()
+        await loggedInPage.addFirstItemToCart()
+        await loggedInPage.goToCart()
+        const cartPage = new CartPage(page);
         await cartPage.expectItemNameVisible(productName)
     });
 })
