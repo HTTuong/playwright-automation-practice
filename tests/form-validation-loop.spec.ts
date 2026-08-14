bd019a7d6c819e732a433bfeb2dfa5fc2fa2a090
Tuong Hoang <httuong.2705@gmail.com>
2026-08-14 22:54:09 +0300

    Refactor form validation with test data

--- tests/form-validation-loop.spec.ts
+++ tests/form-validation-loop.spec.ts
@@ -1,13 +1,14 @@
 import { test } from '../fixtures/pages.fixture';
+import { checkoutInfo } from '../test-data/checkout-info';
 
 const invalidCases = [
-  { firstName: '', lastName: 'Hoang', zip: '00100', expectedError: 'First Name is required' },
-  { firstName: 'Tuong', lastName: '', zip: '00100', expectedError: 'Last Name is required' },
-  { firstName: 'Tuong', lastName: 'Hoang', zip: '', expectedError: 'Postal Code is required' },
+  { data: checkoutInfo.missingFirstName, expectedError: 'First Name is required' },
+  { data: checkoutInfo.missingLastName, expectedError: 'Last Name is required' },
+  { data: checkoutInfo.missingPostalCode, expectedError: 'Postal Code is required' },
 ];
 
-for (const data of invalidCases) {
-  test(`Missing information: ${data.expectedError}`, async ({ loggedInPage, loginPage, cartPage, checkoutPage, page }) => {
+for (const { data, expectedError } of invalidCases) {
+  test(`Missing information: ${expectedError}`, async ({ loggedInPage, loginPage, cartPage, checkoutPage, page }) => {
     await loginPage.goto()
     await loginPage.login('standard_user', 'secret_sauce')
 
@@ -15,11 +16,8 @@ for (const data of invalidCases) {
     await loggedInPage.goToCart()
     await cartPage.checkoutButton.click();
 
-    await checkoutPage.firstNameInput.fill(data.firstName);
-    await checkoutPage.lastNameInput.fill(data.lastName);
-    await checkoutPage.postalCodeInput.fill(data.zip);
-    await checkoutPage.continueButton.click();
-
-    await checkoutPage.expectErrorContains(data.expectedError)
+    await checkoutPage.fillInformation(data.firstName, data.lastName, data.postalCode);
+    await checkoutPage.continueCheckout();
+    await checkoutPage.expectErrorContains(expectedError);
   });
 }
