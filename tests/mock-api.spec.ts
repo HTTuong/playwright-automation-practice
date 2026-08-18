b3eb6ee5b8c7268dcb9e4b6dd19ae51785e2c081
Tuong Hoang <httuong.2705@gmail.com>
2026-08-18 22:36:29 +0300

    Add mock timeout test

--- tests/mock-api.spec.ts
+++ tests/mock-api.spec.ts
@@ -12,3 +12,15 @@ test('Mock error 500 to test error process', async ({ page }) => {
   const response = await page.request.get('https://jsonplaceholder.typicode.com/posts/1');
   expect(response.status()).toBe(500);
 });
+
+test('Mock timeout - request is delayed', async ({ page }) => {
+  await page.route('**/jsonplaceholder.typicode.com/posts/1', async (route) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await route.continue(); // for simulate internet delay
+  });
+
+  const start = Date.now();
+  await page.request.get('https://jsonplaceholder.typicode.com/posts/1');
+  const duration = Date.now() - start;
+  expect(duration).toBeGreaterThanOrEqual(2000);
+});
