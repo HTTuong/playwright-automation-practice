c9b03a2d26cda0d6ec6e1539c01fbc4e48648736
Tuong Hoang <httuong.2705@gmail.com>
2026-08-23 11:33:13 +0300

    Add upload file created in memory test

--- tests/upload-download.spec.ts
+++ tests/upload-download.spec.ts
@@ -10,4 +10,19 @@ test.describe('File Upload', () => {
     await expect(page.locator('#uploaded-files')).toHaveText('sample.txt');
     await expect(page.locator('h3')).toHaveText('File Uploaded!');
   });
+
+
+  test('Upload file created in memory (Buffer), not real file ', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/upload');
+
+    await page.locator('#file-upload').setInputFiles({
+        name: 'generated.txt',
+        mimeType: 'text/plain',
+        buffer: Buffer.from('Content is created in running, not available on disk'),
+    });
+    await page.locator('#file-submit').click();
+
+    await expect(page.locator('#uploaded-files')).toHaveText('generated.txt');
+    });
 });
+
