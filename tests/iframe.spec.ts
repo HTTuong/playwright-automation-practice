36635d84cccb32fefbab6cf9f9bd94657f110e6d
Tuong Hoang <httuong.2705@gmail.com>
2026-08-23 12:04:31 +0300

    Pin allure-commandline package explicitly to fix CI ambiguity

---
+++ tests/iframe.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Iframe handling', () => {
+  test('Fill text into rich text editor in iframe', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/iframe');
+
+    const editorBody = page.frameLocator('#mce_0_ifr').locator('#tinymce');
+
+    await editorBody.click();
+    await editorBody.fill('Content test in written into iframe');
+
+    await expect(editorBody).toHaveText('Content test in written into iframe');
+  });
+
+  test('Delete default text and type a new text ', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/iframe');
+    const editorBody = page.frameLocator('#mce_0_ifr').locator('#tinymce');
+
+    await expect(editorBody).toContainText('Your content goes here');
+
+    await editorBody.click();
+    await editorBody.press('Control+A');
+    await editorBody.press('Delete');
+    await editorBody.type('New Text');
+
+    await expect(editorBody).toHaveText('New Text');
+  });
+});
