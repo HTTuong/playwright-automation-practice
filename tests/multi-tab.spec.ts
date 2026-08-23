32bb78248221e83473e1e0f1df83fb50de99be31
Tuong Hoang <httuong.2705@gmail.com>
2026-08-23 23:16:09 +0300

    Add many tabs test and verify each

--- tests/multi-tab.spec.ts
+++ tests/multi-tab.spec.ts
@@ -18,4 +18,25 @@ test.describe('Multi-tab / window handling', () => {
 
     await newTab.close();
   });
+
+  test('Open many tabs, verify each tab independently', async ({ page, context }) => {
+        await page.goto('https://the-internet.herokuapp.com/windows');
+
+        const [tab1] = await Promise.all([
+            context.waitForEvent('page'),
+            page.getByRole('link', { name: 'Click Here' }).click(),
+        ]);
+        await tab1.waitForLoadState();
+
+        // All Pages is opening in this context
+        const allPages = context.pages();
+        expect(allPages.length).toBe(2); // orginal tab + new tab
+
+        await expect(tab1.locator('h3')).toHaveText('New Window');
+
+        await tab1.close();
+
+        // After closing, context only has 1 page/tab
+        expect(context.pages().length).toBe(1);
+    });
 });
